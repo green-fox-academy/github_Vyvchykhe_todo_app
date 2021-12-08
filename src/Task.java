9b4f0341e3302da41c9f29f72fc941083ab68259
Riger Vivien <vivri66@gmail.com>
2021-12-08 22:22:49 +0100

    todo app folytatása - 2.rész

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Task {
     String taskName;
     boolean ready;
+
+    public static void fileReader(){
+        Path taskFile = Paths.get("mytasks.txt");
+        try {
+            List<String> taskFileLines = Files.readAllLines(taskFile);
+        } catch (IOException e){
+            System.out.println("Nem található a keresett fájl");
+        }
+    }
 }
